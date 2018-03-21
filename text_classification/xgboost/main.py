4b90ff28c574120e449bdadd54c1ab81e87799a6
yyding <idy002@163.com>
2018-03-21 22:06:41 +0800

    all

--- text_classification/xgboost/main.py
+++ text_classification/xgboost/main.py
@@ -6,28 +6,28 @@ import xgboost as xgb
 #
 
 # set tran parameters
-train_file = './data_ok/train.svm.txt'
-# valid_file = 'valid.svm.txt'
+train_file = './data_ok/ctrain.svm.txt'
+valid_file = './data_ok/cvalid.svm.txt'
 test_file = './data_ok/test.svm.txt'
 param = dict()
-param['max_depth'] = 14
-param['max_leaf_nodes'] = 1000
+param['max_depth'] = 5
+param['max_leaf_nodes'] = 50
 param['eta'] = 0.2
 param['silent'] = 1
 param['objective'] = 'binary:logistic';
 param['nthread'] = 4
 param['eval_metric'] = 'auc'
-num_round = 150
+num_round = 400
 
 # read train, valid and test data
 dtrain = xgb.DMatrix(train_file)
-#dvalid = xgb.DMatrix(valid_file)
+dvalid = xgb.DMatrix(valid_file)
 dtest = xgb.DMatrix(test_file)
-evallist = [(dtrain, 'train')]
+evallist = [(dtrain, 'train'), (dvalid,'valid')]
 
 # start training
 bst = xgb.train(param, dtrain, num_round, evallist)
-# bst.save_model('0001.model')
+bst.save_model('0001.model')
 # bst.dump_model('dump.raw.txt')
 
 # start predicting
