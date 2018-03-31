2865b507ea76d98ad5e876f5dbbb6367753e2391
yyding <idy002@163.com>
2018-03-31 16:19:42 +0800

    aa

--- text_classification/xgboost/main.py
+++ text_classification/xgboost/main.py
@@ -6,18 +6,18 @@ import xgboost as xgb
 #
 
 # set tran parameters
-train_file = './data_ok/ctrain.svm.txt'
-valid_file = './data_ok/cvalid.svm.txt'
-test_file = './data_ok/test.svm.txt'
+train_file = '../data_ok/train.svm.txt'
+valid_file = '../data_ok/cvalid.svm.txt'
+test_file = '../data_ok/test.svm.txt'
 param = dict()
-param['max_depth'] = 5
-param['max_leaf_nodes'] = 50
+param['max_depth'] = 4
+param['max_leaf_nodes'] = 40
 param['eta'] = 0.2
 param['silent'] = 1
 param['objective'] = 'binary:logistic';
 param['nthread'] = 4
 param['eval_metric'] = 'auc'
-num_round = 400
+num_round = 2000
 
 # read train, valid and test data
 dtrain = xgb.DMatrix(train_file)
